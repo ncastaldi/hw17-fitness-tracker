575666571678b712e05623c9750eec9cc1298f6e
Nathan Castaldi <nathan@castaldi.dev>
2021-02-05 14:35:54 -0500

    added connection for mongoose

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 // Require external npm packages
 const express = require("express");
+const mongoose = require("mongoose");
 
 // Declare local instances
 const app = express();
 
 // Delcare listening port for Express server
 const PORT = process.env.PORT || 8080;
+
+// Define Mongoose connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+    console.log("Mongoose successfully connected.");
+});
+
+connection.on("error", (err) => {
+    console.log("Mongoose connection error: " + err);
+});
 
 // Express Middleware: Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -17,4 +36,4 @@ app.use(express.json());
 // Starts Express server listening on predefined port
 app.listen(PORT, () => {
     console.log(`Your server is running on http://localhost:${PORT}`);
-})
+});
