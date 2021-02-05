dba185779749072351b4cd069a5d322c244fd88c
Nathan Castaldi <nathan@castaldi.dev>
2021-02-05 16:08:16 -0500

    created view route for stats page

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,12 +1,20 @@
 // Require external npm packages
 const express = require("express");
+const path = require("path");
 
 // Define Express Router
 const router = express.Router();
 
-router.get("/", (function(req, res) {
+// Define HTML View Routes
+router.get("/", (function (req, res) {
     res.render("index");
 }));
+
+router.get("/stats", (function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+}));
+
+// Define API Routes
 
 // Export routes for server.js to use.
 module.exports = router;
