41affb90f79e0f352acb24e3100e981257a5f743
Nathan Castaldi <nathan@castaldi.dev>
2021-02-05 14:29:03 -0500

    updated comments

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+// Require external npm packages
 const express = require("express");
 
+// Declare local instances
 const app = express();
 
+// Delcare listening port for Express server
 const PORT = process.env.PORT || 8080;
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Express Middleware: Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+// Express Middleware: Parses JSON and URL
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Starts Express server listening on predefined port
 app.listen(PORT, () => {
     console.log(`Your server is running on http://localhost:${PORT}`);
 })
