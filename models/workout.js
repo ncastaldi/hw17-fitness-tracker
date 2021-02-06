84124a2abfbe968a782488cb88b9ac2a958dab45
Nathan Castaldi <nathan@castaldi.dev>
2021-02-06 06:44:17 -0500

    updated model to include date

--- models/workout.js
+++ models/workout.js
@@ -3,24 +3,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    type: {
-        type: String
+    day: {
+        type: Date,
+        default: Date.now,
     },
-    name: {
-        type: String
-    },
-    duration: {
-        type: Number
-    },
-    weight: {
-        type: Number
-    },
-    reps: {
-        type: Number
-    },
-    sets: {
-        type: Number
-    }
+    exercises: [
+        {
+            type: {
+                type: String
+            },
+            name: {
+                type: String
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
+    ]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
